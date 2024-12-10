2af59d4bf1359bf88d301b6408c6937ad6b22fda
Darya Melgunova <darya.melgunova@gmail.com>
2024-12-10 16:45:40 +0300

    added dates in DataBase

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class TestData {
+
+    public static String firstName = "Darya";
+    public static String lastName = "Melgunova";
+    public static String userEmail ="darya.melgunova@gmail.com";
+    public static String genterWrapper = "Female";
+    public static String userNumber ="8999543121";
+    public static String monthOfBirth = "March";
+    public static String yearOfBirth = "1996";
+    public static String dayOfBirth = "04";
+    public static String subject = "Computer Science";
+    public static String hobby = "Music";
+    public static String pictureFileName = "380x240.jfif";
+    public static String address = "Street1, City2, Country3";
+    public static String state = "NCR";
+    public static String city = "Delhi";
+}
